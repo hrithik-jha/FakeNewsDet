d0170ef68def111d18260544f181c30c2336094f
Hrithik Jha <hrithik.jha2017@vitstudent.ac.in>
2019-04-08 15:39:53 +0530

    Completed Classifier, Added Similar articles

---
+++ API/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import json
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Sup"
+
+@app.route('/url', methods=["POST"])
+def movie_id():
+    if 'url' in request.args:
+        url = request.args.get('url')
+    else:
+        return "Error: No Id field provided."
+
+    return jsonify({'recommendations': l})
+
+app.run(debug=True)
